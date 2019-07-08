9b2741e7d5028442b2b76800bea99c680d397d43
Brian <briantham1@gmail.com>
2019-07-08 22:18:34 +0800

    Added new images
    Added some images
    Added script to change scene
    Began working on scenes

---
+++ CA2/Assets/Scripts/changeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class changeScene : MonoBehaviour
+{
+    
+    public void ExitGame()
+    {
+        SceneManager.LoadScene(0);  //load the main menu
+    }
+    public void SettingMenu()
+    {
+        SceneManager.LoadScene(1);  //load the settings menu
+    }
+    
+}
