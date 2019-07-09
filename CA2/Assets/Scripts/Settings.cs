3197c42fe142eb711b0f36e33d981dcb92bfd6e6
Brian <briantham1@gmail.com>
2019-07-09 15:11:46 +0800

    Added audio
    Added audioscript as well as audio files
    Music plays on one of the scenes, need to replicate it on other scenes

---
+++ CA2/Assets/Scripts/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        SceneManager.LoadScene(0);  //load the main menu
+    }
+    public void SettingMenu()
+    {
+        SceneManager.LoadScene(1);  //load the settings menu
+    }
+}
